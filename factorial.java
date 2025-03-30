2e2015d65b8dd0f6480c1df5b2b11038d82220c0
joyaditya_nath <joyadityan@gmail.com>
2025-03-30 12:29:11 +0530

    Adding a bunch of files

---
+++ factorial.java
@@ -0,0 +1,16 @@
+// This is a Java code to find the factoril of a number using Recursion
+
+public class Factorial {
+	    public static void main(String[] args) {
+
+		            System.out.println(factorial(5));
+			        }
+
+	        static int factorial(int n) {
+			        if ( n<=2) {
+					            return n;
+						            }
+				        return n* factorial(n - 1);
+
+					    }
+}
